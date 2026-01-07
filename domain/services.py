c4d179321990dd5bf610c1c627fb9fe52f4a5ac5
Miguel-Paes-Pixel <augustomiguelpaes@gmail.com>
2026-01-06 21:24:07 -0300

    feat: implementando alerta via Slack Webhook e arquitetura de Sistema Distribuído

--- domain/services.py
+++ domain/services.py
@@ -5,5 +5,9 @@ def processar_e_salvar_cotacao(valor_dolar):
     if valor_dolar > 0:
         CotacaoRepository.salvar_no_txt(valor_dolar)
         print(f"✅ Sucesso! R$ {valor_dolar} registrado no arquivo histórico.")
+
+        if valor_dolar > 6.00:
+            CotacaoRepository.enviar_para_slack(valor_dolar)
+            print("🚨 Alerta enviado ao Slack!")
     else:
         print("❌ Erro: Valor da cotação inválido.")
--- infrastructure/repository.py
+++ infrastructure/repository.py
@@ -1,4 +1,5 @@
 import datetime
+import requests
 
 class CotacaoRepository:
     @staticmethod
@@ -9,3 +10,8 @@ class CotacaoRepository:
         with open("historico_dolar.txt", "a") as f:
             f.write(f"[{agora}] Valor do Dólar: R$ {valor}\n")
         
+    @staticmethod
+    def enviar_para_slack(valor):
+        url = "https://hooks.slack.com/services/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"
+        payload = {"text": f"⚠️ *Alerta de Cotação* ⚠️\nO Dólar atingiu: *R$ {valor}*"}
+        requests.post(url, json=payload)
